e66b4ba835c7bf31894ab530ec067999c7b695c6
FairuzAH <129727744+FairuzAH@users.noreply.github.com>
2025-03-25 21:49:31 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -85,5 +85,5 @@ df_prediction_prob.columns = ('Adelie', 'Chinstrap', 'Gentoo')
 df_prediction_prob.rename(columns= {0: 'Adelie', 
                                 1: 'Chinstrap', 
                                 2: 'Gentoo'})
-df_prediciton_prob
+df_prediction_prob
 
